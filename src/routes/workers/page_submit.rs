3c6b120c700a744a933426b86c5889e8d1da3de7
Ryan Alexander <w.ryan@stelch.com>
2022-11-11 16:41:30 +1000

    feat: Scrape submit endpoint -- Planned

--- src/routes/workers/page_submit.rs
+++ src/routes/workers/page_submit.rs
@@ -1,4 +1,37 @@
-#[actix_web::post("/workers/page/submit")]
-pub async fn submit_page(db: Data<DbPool>, url: str) -> Result<HttpResponse, AppError> {
-    
+use actix_web::{web, HttpResponse, HttpRequest};
+use serde::Serialize;
+
+use crate::DbPool;
+use crate::infra::logging;
+use crate::model::link_model::NewLink;
+use crate::{repository::link_repository, AppResult};
+
+pub fn query_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(submit_page);
+}
+
+#[actix_web::post("/workers/scrape/submit")]
+pub async fn submit_page(_db: web::Data<DbPool>, request: HttpRequest) -> AppResult<HttpResponse> {
+
+    // Print json body from request
+    logging::log_info(&format!("Received request from {}", request.peer_addr().unwrap().ip()));
+/*
+    logging::log_info(&format!("Received site: {}", site.url));
+
+    let mut tx = db.begin().await.unwrap();
+    link_repository::insert_link(&mut tx, site.url.clone()).await;
+
+    logging::log_debug(&format!("Inserted site: {}", site.url));
+*/
+    let response: GenericOK = GenericOK {
+        success: true
+    };
+
+    Ok(HttpResponse::Ok().json(response))
+
+}
+
+#[derive(Serialize)]
+pub struct GenericOK {
+    pub success: bool
 }
