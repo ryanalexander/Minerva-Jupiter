0e10ec30cd8820da471f23f41ea167f700b3e126
Ryan Alexander <w.ryan@stelch.com>
2022-11-11 16:40:11 +1000

    feat: endpoint to request scrape job from server

--- src/routes/workers/page_query.rs
+++ src/routes/workers/page_query.rs
@@ -1,3 +1,18 @@
-pub async fn req(req: HttpRequest) -> Result<()> {
-    
+use actix_web::{web, HttpResponse};
+
+use crate::DbPool;
+use crate::{repository::link_repository, AppResult};
+
+pub fn query_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(request_page);
+}
+
+// TODO - Streamline this, we should be able to receive either a site or nothing after await
+#[actix_web::get("/workers/scrape/job")]
+pub async fn request_page(db: web::Data<DbPool>) -> AppResult<HttpResponse> {
+    let mut tx = db.begin().await.unwrap();
+    let site = link_repository::fetch_unscraped_link(&mut tx).await.unwrap();
+
+    Ok(HttpResponse::Ok().json(site))
+
 }
