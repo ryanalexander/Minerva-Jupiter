b282dfcb0a6ca6971f98b7edc8828a518aa44ed1
Ryan Alexander <w.ryan@stelch.com>
2022-11-24 18:15:12 +1000

    feat: When inserting site, check if domain is blacklisted

--- src/repository/site_repository.rs
+++ src/repository/site_repository.rs
@@ -8,8 +8,7 @@ use crate::{
 pub async fn insert_site(tx: &mut Tx, new_site: NewSite) -> Result<PgQueryResult, sqlx::Error> {
     let query = sqlx::query!(
         r#"
-        INSERT INTO public."site" (name, url)
-        VALUES ($1, $2)
+        INSERT INTO site (name, url) SELECT * FROM (VALUES ($1, $2)) AS i(name, url) WHERE NOT EXISTS (SELECT hash FROM site_blacklist WHERE hash = md5(i.url)) ON CONFLICT DO NOTHING
         "#,
         new_site.name,
         new_site.url
@@ -18,4 +17,10 @@ pub async fn insert_site(tx: &mut Tx, new_site: NewSite) -> Result<PgQueryResult
     println!("Inserting site: {}", new_site.url);
 
     return query.execute(tx).await;
+}
+
+pub async fn domain_blacklisted(tx: &mut Tx, domain: String) -> bool {
+    let query = sqlx::query!("SELECT hash FROM site_blacklist WHERE hash = md5($1)", domain).fetch_one(tx).await;
+
+    return query.is_ok();
 }
