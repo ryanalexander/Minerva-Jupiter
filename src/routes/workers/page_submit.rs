62cfce4fc720fe45f8cbe389c977fa0f698baffc
Ryan Alexander <w.ryan@stelch.com>
2022-11-24 18:20:52 +1000

    feat: Ability to submit bulk urls to Jupiter

--- src/routes/workers/page_submit.rs
+++ src/routes/workers/page_submit.rs
@@ -1,31 +1,28 @@
 use actix_web::{web, HttpRequest, HttpResponse};
 use serde::Serialize;
-
-use crate::infra::logging;
 use crate::DbPool;
 
 use crate::AppResult;
+use crate::model::link_model::NewLink;
+use crate::repository::link_repository;
 
 pub fn query_config(cfg: &mut web::ServiceConfig) {
     cfg.service(submit_page);
 }
 
 #[actix_web::post("/workers/scrape/submit")]
-pub async fn submit_page(_db: web::Data<DbPool>, request: HttpRequest) -> AppResult<HttpResponse> {
-    // Print json body from request
-    logging::log_info(&format!(
-        "Received request from {}",
-        request.peer_addr().unwrap().ip()
-    ));
-    /*
-        logging::log_info(&format!("Received site: {}", site.url));
+pub async fn submit_page(db: web::Data<DbPool>, json: web::Json<Vec<NewLink>>) -> AppResult<HttpResponse> {
 
-        let mut tx = db.begin().await.unwrap();
-        link_repository::insert_link(&mut tx, site.url.clone()).await;
+    let mut tx = db.begin().await.unwrap();
+    for link in json.iter() {
+        let link = link.clone();
+        link_repository::insert_link(&mut tx, link.url).await.unwrap();
+    }
+    tx.commit().await.ok();
 
-        logging::log_debug(&format!("Inserted site: {}", site.url));
-    */
-    let response: GenericOK = GenericOK { success: true };
+    let response: GenericOK = GenericOK {
+        success: true
+    };
 
     Ok(HttpResponse::Ok().json(response))
 }
